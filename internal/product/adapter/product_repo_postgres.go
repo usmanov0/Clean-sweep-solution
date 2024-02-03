29d56a648ad1bf1d0524553c9503d260e31760c6
islom <ismoilovislom589@gmail.com>
2024-02-04 03:30:39 +0500

    add insert method

--- internal/product/adapter/product_repo_postgres.go
+++ internal/product/adapter/product_repo_postgres.go
@@ -16,5 +16,10 @@ func NewProductRepo(db *pgx.Conn) domain.ProductRepository {
 	return &productRepo{db: db}
 }
 
+func (p *productRepo) Insert(ctx context.Context, product pb.ProductRequest) error {
+	_, err := p.db.Exec("INSERT INTO products (name, price, count, created_at) values ($1,$2,$3,$4)",
+		product.Name, int(product.Price), int(product.Count), product.CreatedAt)
 
+	return err
+}
 

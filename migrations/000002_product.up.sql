aafe8af2a1dfe407055934f621b1349246bb1527
islom <ismoilovislom589@gmail.com>
2024-02-03 22:56:43 +0500

    add field

--- migrations/000002_product.up.sql
+++ migrations/000002_product.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE "products" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR,
     "price" int,
+    "count" int,
     "created_at" TIMESTAMP DEFAULT current_timestamp,
     "updated_at" TIMESTAMP DEFAULT current_timestamp,
     "deleted_at" TIMESTAMP

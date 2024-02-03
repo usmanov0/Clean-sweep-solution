02883b048af00208b61f7719db6d1f3f1a08197b
usmanov0 <azikusmanov882@gmail.com>
2024-02-03 20:24:07 +0500

    added :usecase

--- internal/user/app/user_usecase.go
+++ internal/user/app/user_usecase.go
@@ -1,1 +1,26 @@
 package app
+
+import "Clean-Sweep-Solutions_/internal/user/domain"
+
+type userUseCase struct {
+	userRepo domain.UserRepository
+}
+
+type UserUseCase interface {
+	SignUpAdmin(user *domain.User) error
+	SignUpUser(user *domain.User) error
+}
+
+func NewUserUseCase(userRepo domain.UserRepository) UserUseCase {
+	return userUseCase{userRepo: userRepo}
+}
+
+func (u userUseCase) SignUpAdmin(user *domain.User) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (u userUseCase) SignUpUser(user *domain.User) error {
+	//TODO implement me
+	panic("implement me")
+}

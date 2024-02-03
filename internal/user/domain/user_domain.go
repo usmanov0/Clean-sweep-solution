2bc27a7add6eb43f486f7e3cc4c1dcd3f2da9c5e
usmanov0 <azikusmanov882@gmail.com>
2024-02-03 20:23:09 +0500

    added :user domain with repository Save function

--- internal/user/domain/user_domain.go
+++ internal/user/domain/user_domain.go
@@ -1,1 +1,25 @@
 package domain
+
+import (
+	"github.com/lib/pq"
+	"time"
+)
+
+type UserRole string
+
+const (
+	adminRole UserRole = "admin"
+	userRole  UserRole = "user"
+)
+
+type User struct {
+	Id        int
+	FullName  string
+	Email     string
+	Phone     string
+	Password  string
+	Role      UserRole
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt pq.NullTime
+}

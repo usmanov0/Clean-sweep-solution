df00d0ec47c53f5a3738c43ce60e040fceff826a
islom <ismoilovislom589@gmail.com>
2024-02-04 03:34:27 +0500

    add

--- internal/product/app/product_usecase.go
+++ internal/product/app/product_usecase.go
@@ -1,1 +1,38 @@
 package app
+
+import (
+	"Clean-sweep-solution/internal/genproto/product/pb"
+	"Clean-sweep-solution/internal/product/domain"
+	"context"
+	"time"
+
+	"github.com/golang/protobuf/ptypes/timestamp"
+)
+
+
+type ProductUseCase interface {
+	Create(context.Context, pb.ProductRequest) error
+}
+
+func NewProductUseCase(repo domain.ProductRepository) ProductUseCase {
+	return &productUseCase{repo: repo}
+}
+
+type productUseCase struct {
+	repo domain.ProductRepository
+}
+
+func (p *productUseCase) Create(ctx context.Context, product pb.ProductRequest) error {
+	product.CreatedAt = &timestamp.Timestamp{
+		Seconds: time.Now().Unix(),
+		Nanos:   int32(time.Now().Nanosecond()),
+	}
+
+	err := p.repo.Insert(ctx, product)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}

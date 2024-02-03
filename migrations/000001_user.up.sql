4b9cc1c63975bb1aa7fbe3432c09df3d0bf13d95
usmanov0 <azikusmanov882@gmail.com>
2024-02-03 20:22:33 +0500

    added :migrations

---
+++ migrations/000001_user.up.sql
@@ -0,0 +1,13 @@
+CREATE TYPE user_role AS ENUM ('admin', 'user');
+
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
+    "full_name" VARCHAR,
+    "email" VARCHAR,
+    "phone" VARCHAR,
+    "password" VARCHAR,
+    "role" user_role,
+    "created_at" TIMESTAMP DEFAULT current_timestamp,
+    "updated_at" TIMESTAMP DEFAULT current_timestamp,
+    "deleted_at" TIMESTAMP
+);

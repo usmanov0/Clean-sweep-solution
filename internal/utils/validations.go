0a3497f69c5c58719fa792515768a81c59462c0e
usmanov0 <azikusmanov882@gmail.com>
2024-02-03 21:34:45 +0500

    added :validations

---
+++ internal/utils/validations.go
@@ -0,0 +1,100 @@
+package utils
+
+import (
+	"Clean-Sweep-Solutions_/internal/errors"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"math/rand"
+	"regexp"
+	"strings"
+)
+
+func ValidateUserInfoForSignUp(fullName, email, phone, password string) error {
+
+	if strings.TrimSpace(fullName) == "" {
+		return errors.ErrEmptyName
+	}
+	if strings.TrimSpace(email) == "" {
+		return errors.ErrEmptyMail
+	}
+	if !isValidPhoneNumber(phone) {
+		return errors.ErrInvalidPhoneNumber
+	}
+	if ValidatePassword(password) == errors.ErrInvalidPassword {
+		return errors.ErrInvalidPassword
+	}
+	return nil
+}
+
+func ValidateUserInfoForSignIn(email, password string) error {
+	if strings.TrimSpace(email) == "" {
+		return errors.ErrBadCredentials
+	}
+	if ValidatePassword(password) == errors.ErrInvalidPassword {
+		return errors.ErrBadCredentials
+	}
+	return nil
+}
+
+func ValidatePassword(password string) error {
+	if len(password) < 8 {
+		return errors.ErrInvalidPassword
+	}
+
+	var (
+		upperCase = regexp.MustCompile(`[A-Z]`)
+		lowerCase = regexp.MustCompile(`[a-z]`)
+		digit     = regexp.MustCompile(`[0-9]`)
+	)
+	if !upperCase.MatchString(password) || !lowerCase.MatchString(password) {
+		return errors.ErrInvalidPassword
+	}
+	if !digit.MatchString(password) {
+		return errors.ErrInvalidPassword
+	}
+	return nil
+}
+
+func isValidPhoneNumber(phone string) bool {
+	if len(phone) < 12 {
+		fmt.Sprintf("error: phone number count is more than 12")
+		return false
+	}
+	phoneRegex := `^\+?[1-9]\d{1,14}$|^[1-9]\d{0,14}$|^[1-9]\d{0,14}-[1-9]\d{0,14}$`
+
+	regExp := regexp.MustCompile(phoneRegex)
+
+	return regExp.MatchString(phone)
+}
+
+func ValidateEmail(email string) error {
+	emailReg := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	if !emailReg.MatchString(email) {
+		return errors.ErrInvalidEmailFormat
+	}
+	return nil
+}
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashedPassword), nil
+}
+
+func CheckPassword(password, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func RandomPassword() string {
+	var digitBytes string = "0123456789"
+
+	password := make([]byte, 4)
+	for i := range password {
+		password[i] = digitBytes[rand.Intn(len(digitBytes))]
+	}
+
+	return string(password)
+}

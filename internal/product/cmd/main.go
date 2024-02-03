ad610647620eb731f16ac81fe0608a3247418208
islom <ismoilovislom589@gmail.com>
2024-02-04 03:18:16 +0500

    add connection db

---
+++ internal/product/cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"Clean-sweep-solution/internal/product/adapter"
+	"Clean-sweep-solution/internal/product/app"
+	"Clean-sweep-solution/pkg/common"
+	server "Clean-sweep-solution/internal/product/delivery/grpc"
+	"log"
+	"os"
+)
+
+func main() {
+	db, err := common.ConnectToDb(
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_DATABASE"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+	)
+	if err != nil {
+		log.Printf("Failed to connect to database: %v", err)
+	}
+
+	defer db.Close()
+
+
+}

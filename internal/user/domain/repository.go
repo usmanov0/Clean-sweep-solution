ae6a6fb82bfa729adaf282cdc361ba2bb2f376b7
usmanov0 <azikusmanov882@gmail.com>
2024-02-04 12:29:12 +0500

    added  with implementation :GetUsers, FindById, UpdateUser, DeleteUser

--- internal/user/domain/repository.go
+++ internal/user/domain/repository.go
@@ -3,4 +3,8 @@ package domain
 type UserRepository interface {
 	Save(user *User) error
 	UserExistByEmail(email string) (bool, error)
+	GetUsers() ([]User, error)
+	FindById(userId int) (*User, error)
+	UpdateUser(user *User) error
+	DeleteUser(userId int) error
 }

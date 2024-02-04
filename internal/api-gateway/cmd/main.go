39219d0a5ebf64a546ea26ae19dd8286292d7a02
islom <ismoilovislom589@gmail.com>
2024-02-04 05:15:45 +0500

    called newclientConstruction from our package

--- internal/api-gateway/cmd/main.go
+++ internal/api-gateway/cmd/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	grpc_client "Clean-sweep-solution/internal/api-gateway/controller/grpc"
+	"Clean-sweep-solution/internal/api-gateway/controller/rest"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+// @title Clean-sweep-solution_App 
+// @version 1.0
+// @description API Server for Clean-sweep-solution Application
+
+// @host localhost:8000
+// @BasePath /
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+func main() {
+	auditClient, err := grpc_client.NewClient(os.Getenv("PRODUCT_PORT"))
+	if err != nil {
+		log.Println(err, "port")
+	}
+	defer auditClient.CloseConnection()
+
+	
+}
--- internal/api-gateway/controller/http/handler.go
+++ internal/api-gateway/controller/http/handler.go
@@ -1,5 +1,58 @@
 package rest
 
-type Handler struct{
-	
+import (
+	grpc_client "Clean-sweep-solution/internal/api-gateway/controller/grpc"
+	"Clean-sweep-solution/internal/genproto/product/pb"
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	productClient grpc_client.Client
+}
+
+func NewHandler(client grpc_client.Client) *Handler {
+	return &Handler{
+		productClient: client,
+	}
+}
+
+// @Summary Create a new product
+// @Security ApiKeyAuth
+// @Description Create a new product with the provided JSON data
+// @Tags products
+// @Accept json
+// @Produce json
+// @Param product body ProductRequest true "Product object that needs to be created"
+// @Success 201 {object} SuccessResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /api/products [post]
+func (h *Handler) CreateProduct(c *gin.Context) {
+	reqBytes, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to read request body"})
+		return
+	}
+
+	var product pb.ProductRequest
+	if err = json.Unmarshal(reqBytes, &product); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to unmarshal JSON"})
+		return
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = h.productClient.CreateProduct(ctx, product)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create product"})
+		return
+	}
+
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusCreated, gin.H{"message": "Product created successfully"})
 }

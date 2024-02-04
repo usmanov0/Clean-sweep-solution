2f3b2cef1ddac413520f09dc32c20bae56c7ff78
usmanov0 <azikusmanov882@gmail.com>
2024-02-04 12:31:27 +0500

    added logic: SignInUser, GetUsers, GetUserById, Update(fullname,phone,password,updated_at), DeteleUser

--- internal/user/app/user_usecase.go
+++ internal/user/app/user_usecase.go
@@ -1,11 +1,12 @@
 package app
 
 import (
+	"Clean-Sweep-Solutions_/internal/errors"
 	"Clean-Sweep-Solutions_/internal/user/domain"
 	"Clean-Sweep-Solutions_/internal/utils"
-	"errors"
 	"fmt"
 	"log"
+	"time"
 )
 
 type userUseCase struct {
@@ -16,6 +17,11 @@ type userUseCase struct {
 type UserUseCase interface {
 	SignUpAdmin(user *domain.NewUser) error
 	SignUpUser(user *domain.NewUser) error
+	SignInUser(email, password string) (bool, error)
+	GetUsers() ([]domain.User, error)
+	GetUser(userId int) (*domain.User, error)
+	Update(user *domain.User) error
+	Delete(userId int) error
 }
 
 func NewUserUseCase(userRepo domain.UserRepository) UserUseCase {
@@ -33,7 +39,8 @@ func (u *userUseCase) SignUpAdmin(admin *domain.NewUser) error {
 	)
 
 	if err != nil {
-		fmt.Sprintf("can't sign up admin")
+		fmt.Sprintf("failed sign up admin")
+		return err
 	}
 
 	exists, err := u.userRepo.UserExistByEmail(adminUser.Email)
@@ -43,7 +50,7 @@ func (u *userUseCase) SignUpAdmin(admin *domain.NewUser) error {
 	}
 
 	if exists {
-		return errors.New("email already exists")
+		return errors.ErrEmailExist
 	} else {
 		err := u.userRepo.Save(adminUser)
 		if err != nil {
@@ -54,5 +61,96 @@ func (u *userUseCase) SignUpAdmin(admin *domain.NewUser) error {
 }
 
 func (u *userUseCase) SignUpUser(user *domain.NewUser) error {
+	userSignUp := u.userFactory.CreateUser(user)
+
+	err := utils.ValidateUserInfoForSignIn(
+		userSignUp.Email,
+		userSignUp.Password,
+	)
+
+	if err != nil {
+		fmt.Sprintf("failed sign up user")
+		return err
+	}
+
+	exists, err := u.userRepo.UserExistByEmail(userSignUp.Email)
+	if err != nil {
+		log.Println("internal error: " + err.Error())
+		return err
+	}
+
+	if exists {
+		return errors.ErrEmailExist
+	} else {
+		err := u.userRepo.Save(userSignUp)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func (u *userUseCase) SignInUser(email, password string) (bool, error) {
+	err := utils.ValidateUserInfoForSignIn(email, password)
+
+	if err != nil {
+		return false, nil
+	}
+
+	ok, err := u.userRepo.UserExistByEmail(email)
+	if !ok || err != nil {
+		return false, errors.ErrUserNotFound
+	}
+
+	return true, nil
+}
+
+func (u *userUseCase) GetUsers() ([]domain.User, error) {
+	userList, err := u.userRepo.GetUsers()
+
+	if err != nil {
+		return nil, errors.ErrUserNotFound
+	}
+
+	return userList, nil
+}
+
+func (u *userUseCase) GetUser(userId int) (*domain.User, error) {
+	user, err := u.userRepo.FindById(userId)
+
+	if err != nil {
+		return nil, errors.ErrUserNotFound
+	}
+
+	return user, nil
+}
+
+func (u *userUseCase) Update(userUpdate *domain.User) error {
+	var id int
+	existUser, err := u.userRepo.FindById(id)
+
+	if err != nil {
+		return errors.ErrUserNotFound
+	}
+	existUser.FullName = userUpdate.FullName
+	existUser.Phone = userUpdate.Phone
+	existUser.Password = userUpdate.Password
+	existUser.UpdatedAt = time.Now()
+
+	err = u.userRepo.UpdateUser(existUser)
+	if err != nil {
+		return errors.ErrUpdateFailed
+	}
+
+	return nil
+}
+
+func (u *userUseCase) Delete(userId int) error {
+	err := u.userRepo.DeleteUser(userId)
+
+	if err != nil {
+		return errors.ErrUserDeleteFailed
+	}
+
 	return nil
 }

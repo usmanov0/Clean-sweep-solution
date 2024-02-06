79840d82d8a490796ada9c7d81c07808f8cfb677
usmanov0 <azikusmanov882@gmail.com>
2024-02-06 14:20:30 +0500

    repository :get from pb impl :changes same logic

--- internal/user/domain/repository.go
+++ internal/user/domain/repository.go
@@ -5,8 +5,8 @@ import "example.com/m/internal/genproto/user_pb/pb"
 type UserRepository interface {
 	Save(user *User) error
 	UserExistByEmail(email string) (bool, error)
-	GetUsers() ([]pb.User, error)
-	FindById(id *pb.UserId) (*pb.User, error)
+	GetUsers(*pb.UserRequest) (*pb.UsersResponse, error)
+	FindById(id *pb.UserId) (*pb.UserResponse, error)
 	GetHashedPasswordByEmail(email string) (string, error)
 	UpdateUser(user *pb.UserUpdate) error
 	DeleteUser(id *pb.UserId) error

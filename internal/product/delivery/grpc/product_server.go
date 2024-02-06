17fe116d61f7bd1a9af4314c8e04be6daf059643
islom <ismoilovislom589@gmail.com>
2024-02-06 13:57:16 +0500

    add

--- internal/product/delivery/grpc/product_server.go
+++ internal/product/delivery/grpc/product_server.go
@@ -18,7 +18,6 @@ func NewProductServer(service app.ProductUseCase) productServer {
 }
 
 func (p *productServer) CreateProduct(ctx context.Context, product *pb.ProductRequest) (*pb.EmptyResponse, error) {
-
 	return &pb.EmptyResponse{}, p.service.Create(ctx, *product)
 }
 

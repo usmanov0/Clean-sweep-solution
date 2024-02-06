122b1dbfffc1e690b2ec9ab5557a0d0601b0d1d2
islom <ismoilovislom589@gmail.com>
2024-02-06 13:58:19 +0500

    add

--- internal/product/domain/repository.go
+++ internal/product/domain/repository.go
@@ -8,7 +8,7 @@ import (
 type ProductRepository interface {
 	Insert(context.Context, pb.ProductRequest) error
 	GetByID(ctx context.Context,ID int)(pb.ProductResponse,error)
-	UpdateByID(ctx context.Context, productInp pb.UpdateProductRequest)error
+	UpdateByID(ctx context.Context, Id int, productInp ProductUpdate) error
 	DeleteByID(ctx context.Context, id int)error
 	GetPage(offset, limit int) (pb.ProductResponseList, error)
 }
